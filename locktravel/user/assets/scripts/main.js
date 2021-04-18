1a35ad495a1f56f756da3528cea618f2cdf49d72
Kunal Kumar Gupta <42343749+kunal817@users.noreply.github.com>
2021-04-19 00:58:40 +0530

    Add files via upload

---
+++ locktravel/user/assets/scripts/main.js
@@ -0,0 +1,24 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("nav").style.padding = "12px 0px";
+    document.getElementsByClassName("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("nav").style.padding = "20px 0px";
+    document.getElementsByClassName("logo").style.fontSize = "35px";
+  }
+}
+
+function packageFun(){
+  alert("To book this package, you need to Login First");
+}
+
+function paymentFun(){
+  alert("Payment Successful. Thank you for booking this Tour package :)")
+
+}
+
+function ContactFun(){
+  alert("Thank you for contacting us. We will reach to you as soon as possible.");
+}
